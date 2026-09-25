5d340d66163d91504f5a200ca74fcf6808433837
AlexTan <tanalex0228@gmail.com>
2026-09-26 01:22:07 +0800

    Add concise deployment login aliases

--- doctoral-publication-project-bootstrap/config/urls.py
+++ doctoral-publication-project-bootstrap/config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.views.generic import RedirectView
 from django.urls import include, path
+from accounts import views as account_views
 from .views import healthz
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", account_views.login_view, name="deployment-login"),
+    path("logout/", account_views.logout_view, name="deployment-logout"),
     path("accounts/", include("accounts.urls")),
     path("healthz", healthz, name="healthz"),
     path("dashboard/", include("dashboard.urls")),
